3d3bf98d0727cc12b0e31c641af9ac5b72b28082
Eric Berry <eric@berry.sh>
2024-01-15 14:15:40 -0700

    initial commit

---
+++ lib/debounceable.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "debounceable/version"
+
+module Debounceable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/debounceable/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Debounceable
+  VERSION = "0.1.0"
+end
